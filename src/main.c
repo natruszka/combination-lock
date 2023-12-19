8b80e9b90e6774a56e7661ec0e5e4400d22f2aec
Aleksandra Michalska <92887541+natruszka@users.noreply.github.com>
2023-12-19 17:46:33 +0100

    lab19.12

--- src/main.c
+++ src/main.c
@@ -2,9 +2,20 @@
 #include "touchscreen_LCD.h"
 #include "UART.h"
 
+static void touchpanelDelayUS(uint32_t cnt) {
+	volatile uint32_t i;
+	for (i = 0; i < cnt; i++) {
+		volatile uint8_t us = 12; /*  */
+		while (us--) /*  */
+		{
+			;
+		}
+	}
+}
 int main ( void )
 {
 	UART_init ();
+	send ( "ACHTUNG\r\n" );
 	touchpanelInit ();
 	send ( "START\r\n" );
 	lcdConfiguration ();
@@ -12,16 +23,25 @@ int main ( void )
 	lcdWriteReg ( ADRX_RAM, 0 );
 	lcdWriteReg ( ADRY_RAM, 0 );
 	lcdWriteIndex ( DATA_RAM );
+	touchpanelDelayUS(40);
 	for ( int register x = 0; x < LCD_MAX_X * LCD_MAX_Y; x++ )
 	{
 		lcdWriteData ( LCDBlueSea );
 	}
-	lcdDrawConfigTouchpanel ();
-	lcdDrawInterface ();
+	//lcdDrawConfigTouchpanel ();
+	//lcdDrawInterface ();
 //	lcdTouchscreenCallibrate ();
-	
-	send("Hello World!\n\r");
 
+	//	char temp[10];
+	//	sprintf(temp, "%d", LCD_MAX_Y);
+	//	send(temp);
+
+	send("I'm free\r\n");
+	char imie[20];
+	sprintf(imie, "%d", LCD_MAX_X);
+	send(imie);
+	sprintf(imie, "%d", LCD_MAX_Y);
+	send(imie);
 	while ( 1 )
 	{
 	}
